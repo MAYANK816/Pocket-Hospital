95f71c764c152b9c5be85b9529168255e79ad785
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2022-01-31 22:54:24 +0530

    Pocket Hospital Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './Components/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import About from './Components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Footer from './Components/Footer';
+const App = () => {
+  return <>
+ 
+    <Router>
+      <div>
+      <Navbar/>
+      <Routes>
+     <Route exact path="/" element={<Home />}/>
+          <Route exact path="/about" element={<About />}/>
+          <Route exact path="/contact" element={ <Contact />}/> 
+      </Routes>
+    <Footer/>
+      </div>
+    </Router>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  </>;
+};
 
 export default App;
